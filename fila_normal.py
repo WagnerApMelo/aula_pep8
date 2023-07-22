216d411e448b31e877019ad8372d50a283a7ead6
WagnerApMelo <wagnermelo36@yahoo.com.br>
2023-07-22 11:43:09 -0300

    init commit

---
+++ fila_normal.py
@@ -0,0 +1,16 @@
+from fila_base import FilaBase
+
+class FilaNormal(FilaBase):
+    
+    def gera_senha_atual(self)->None:
+        self.senha_atual = f'NM{self.codigo}'
+        
+    def atualiza_fila(self)->None:
+        self.reseta_fila()
+        self.gera_senha_atual()
+        self.fila.append(self.senhaatual)
+
+    def chama_cliente(self, caixa:int)->str:
+        cliente_atual = self.fila.pop(0)
+        self.clintes_atendidos.append(cliente_atual)
+        return f'Cliente atual: {cliente_atual}, dirija-se ao caixa {caixa}'
